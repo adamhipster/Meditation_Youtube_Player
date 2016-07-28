66cca637190db93d007a414105cdb12eb2fad848
mettamage <mettamage@users.noreply.github.com>
2016-07-28 09:56:21 +0200

    updated messages are working

--- meditation_player.js
+++ meditation_player.js
@@ -20,7 +20,7 @@ function onPlayerReady(event) {
 }
 
 function setSecondVideoTime() {
-        player.mute();
+    player.mute();
     player.cueVideoById(ids[1]);
     player.playVideo();
     setTimeout(function () {
@@ -33,13 +33,12 @@ function setSecondVideoTime() {
 
 //http://stackoverflow.com/questions/3733227/javascript-seconds-to-minutes-and-seconds
 function fromSecondsToFormattedTime(time){
-  var minutes = Math.floor(time / 60);
-  var seconds = time - minutes * 60;
-  function str_pad_left(string,pad,length) {
-    return (new Array(length+1).join(pad)+string).slice(-length);
-  }
-  var finalTime = str_pad_left(minutes,'0',2)+':'+str_pad_left(seconds,'0',2);
-  return finalTime;
+    var parsedTime = CountDownTimer.parse(time);
+    function str_pad_left(string,pad,length) {
+        return (new Array(length+1).join(pad)+string).slice(-length);
+    }
+    var finalTime = str_pad_left(parsedTime.minutes,'0',2)+':'+str_pad_left(parsedTime.seconds,'0',2);
+    return finalTime;
 }
 
 //Timer part from: http://stackoverflow.com/questions/20618355/the-simplest-possible-javascript-countdown-timer
@@ -47,11 +46,11 @@ window.onload = function () {
     var display = document.querySelector('#time');
     document.querySelector('button').addEventListener('click', function () { 
         var timer = new CountDownTimer(countdown_time);
-      var timeObj = CountDownTimer.parse(countdown_time);
-      format(timeObj.minutes, timeObj.seconds);
-      timer.onTick(format).onTick(playSongs);
-      timer.start(); 
-      console.log(vid_times);
+        var timeObj = CountDownTimer.parse(countdown_time);
+        format(timeObj.minutes, timeObj.seconds);
+        timer.onTick(format).onTick(playSongs);
+        timer.start(); 
+        console.log(vid_times);
     });
     
     function format(minutes, seconds) {
@@ -63,15 +62,15 @@ window.onload = function () {
 // own functions start from here
     function playSongs(){
         if(display.innerHTML == vid_times[0]){
-        startSong(ids[0]);
-      }
-      if(display.innerHTML == vid_times[1]){
-        startSong(ids[1]);
-      }
+            startSong(ids[0]);
+        }
+        if(display.innerHTML == vid_times[1]){
+            startSong(ids[1]);
+        }
     }
     
     function startSong(id){
-            player.unMute();
+        player.unMute();
         player.cueVideoById(id);
         player.playVideo();
     }
@@ -80,18 +79,25 @@ window.onload = function () {
 
 function submitVideoIds(vid1, vid2){
     ids = [vid1, vid2];
-  setSecondVideoTime();
-  console.log(ids);
+    setSecondVideoTime();
+    update("updated_ids", "video_ids", ids);
 }
 
 function changeCountDownTime(time){
-  vid_times[0] = fromSecondsToFormattedTime(time);
-  countdown_time = time;
-  console.log(vid_times, countdown_time);
+    vid_times[0] = fromSecondsToFormattedTime(time);
+    countdown_time = time;
+    update_var = fromSecondsToFormattedTime(time);
+    update("updated_time", "video_times", update_var);
 }
 
-//submitVideoTimes deprecated
-function submitVideoTimes(vid_time1, vid_time2){
-    vid_times = [vid_time1, vid_time2];
-  console.log(vid_times);
+function update(element_id, hook_element_id, update_var){
+    var div = document.createElement('div');
+    div.setAttribute("id", element_id);
+    var text = document.createTextNode("updated: " + update_var);
+    div.appendChild(text);
+    var hook_element = document.getElementById(hook_element_id).nextSibling;
+    var parent_node = hook_element.parentNode;
+    var found_div = document.getElementById(element_id);
+    parent_node.insertBefore(div, hook_element);
+    if(found_div != null){ parent_node.removeChild(found_div); }
 }
